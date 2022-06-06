6a641cdbabe97a2d4762360da440a308c2bbb491
Timo Talvitie <timo.j.talvitie@gmail.com>
2022-06-06 14:26:39 +0300

    Swap lightbox lib to simpleLightbox; add first portfolio items

--- js/script.js
+++ js/script.js
@@ -1,14 +1,70 @@
 
 let currentSlideIndex;
 
+const portfolio = [
+  {
+    "href": "portfolio/p_the_mandalorian.jpg",
+    "title": `
+      <b>The Mandalorian Poster Idea</b>.
+      A mashup of movie posters, made in the style of <u>The American</u> but with Mandalorian characters. <a href="www.google.fi">link test</a> lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum end.
+      `
+  },
+  {
+    "href": "portfolio/p_minion_peaks.jpg",
+    "title": `
+      <b>Twin Peaks / Minions Poster Mashup</b>.
+      A mashup of movie posters, made in the style of <u>Twin Peaks</u> but with inions.
+      `
+  },
+  {
+    "href": "portfolio/p_max_beane.jpg",
+    "title": `
+      <b>Max Payne / Mr. Bean Mashup</b>.
+      A mashup of video game art in the style of Max Payne, merged with Mr. Bean.
+      `
+  }    
+];
+
+const pItemGradient = "linear-gradient(rgba(0, 0, 0, 0.55), rgba(0, 0, 0, 0.20))";
+// const pItemGradient = "linear-gradient(rgba(255, 255, 255, 0.75), rgba(255, 255, 255, 0.20))";
+// const pItemGradient = "linear-gradient(rgba(150, 150, 150, 0.65), rgba(150, 150, 150, 0.20))";
+
 $(document).ready(() => {
 
     currentSlideIndex = 0;
     $("#menu-item-bio").on("click", scrollUp);
     $("#menu-item-portfolio, #text-link-portfolio").on("click", scrollDown);
 
-    $('.test-popup-link').magnificPopup({
-      type: 'image'
+    for(const p of portfolio) {
+
+      // Assume all portfolio images have the _thumb suffix.
+      const thumbnailPath = `${p.href.split(".")[0]}_thumb.jpg`;
+
+      const $pItem = $("<a></a>", {
+        "class": "portfolio-item m-1",
+        "href": p.href,
+        "sbl-title": p.title,
+        "data-thumbnail-path": thumbnailPath
+      })
+      
+      $pItem.css("background-image", `${pItemGradient}, url(${thumbnailPath})`);
+      $pItem.appendTo("#portfolio-container");
+        
+      $pItem.hover(
+        function() {
+          const thumbPath = $(this).data("thumbnail-path");
+          $(this).css("background-image", `url(${thumbPath})`);
+        }, function() {
+          const thumbPath = $(this).data("thumbnail-path");
+          $(this).css("background-image", `${pItemGradient}, url(${thumbPath})`);
+        }
+      );
+    }
+
+    // Init the gallery
+    new SimpleLightbox({
+      elements: '#portfolio-container a',
+      captionAttribute: "sbl-title"
     });
 });
 
